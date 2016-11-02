9ac1bd5a05b2ca04b8fba98567562ca351772fb7
remoteEnterprise <reremoteenterprise@gmail.com>
2016-11-02 09:03:10 -0300

    StaticQueue

--- src/staticqueue/QueueEmptyException.java
+++ src/staticqueue/QueueEmptyException.java
@@ -1,4 +1,4 @@
-package queue;
+package staticqueue;
 
 public class QueueEmptyException extends Exception {
 
--- src/staticqueue/QueueFullException.java
+++ src/staticqueue/QueueFullException.java
@@ -1,4 +1,4 @@
-package queue;
+package staticqueue;
 
 public class QueueFullException extends Exception {
 
